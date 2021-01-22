3ad5ee8e845791e9806c532228646903530c39ee
Ivaylo Nikolov <inikolov@sonicomit.com>
2021-01-22 11:04:50 +0200

    LControlBigImage.vue, example.vue, readme update

---
+++ LControlBigImage.vue
@@ -0,0 +1,25 @@
+<script>
+import { OptionsMixin, ControlMixin, propsBinder, optionsMerger } from 'vue2-leaflet';
+import 'leaflet.bigimage/dist/Leaflet.BigImage.min.js';
+import 'leaflet.bigimage/dist/Leaflet.BigImage.min.css';
+export default {
+  name: 'LControlBigImage',
+  mixins: [
+    ControlMixin,
+    OptionsMixin,
+  ],
+  mounted () {
+   const options = optionsMerger({
+      ...this.controlOptions,
+      options: this.options,
+    }, this);
+
+    this.mapObject = new L.Control.BigImage(options);
+    propsBinder(this, this.mapObject, this.$options.props);
+    this.mapObject.addTo(this.$parent.mapObject);
+  },
+  render () {
+    return null;
+  },
+};
+</script>
---
+++ example.vue
@@ -0,0 +1,45 @@
+<template>
+  <div style="height: 100%; width: 100%">
+    <l-map
+      style="height: 400px"
+      :center="mapCentre"
+      :zoom="mapZoom"
+      ref="map"
+    >
+      <l-tile-layer
+        :url="tileUrl"
+        :attribution="tileAttribution"
+      />
+      <l-control-big-image />
+    </l-map>
+  </div>
+</template>
+
+<script>
+import L from 'leaflet';
+import { LMap, LTileLayer } from 'vue2-leaflet';
+import LControlBigImage from './LControlBigImage.vue';
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+    LControlBigImage,
+  },
+  data () {
+    return {
+      mapCentre: [51, -114],
+      mapZoom: 10,
+      tileUrl: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      tileAttribution: '&copy; <a href="//osm.org/copyright">OpenStreetMap</a> contributors',
+      
+    };
+  },
+  methods: {
+    
+  }
+};
+</script>
+
+<style>
+@import '~leaflet/dist/leaflet.css';
+</style>
