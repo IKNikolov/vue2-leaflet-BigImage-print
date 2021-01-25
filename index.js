34f82d0e9b80c5cc26620f1f2ef29aed0882c86c
Ivaylo Nikolov <inikolov@sonicomit.com>
2021-01-25 11:43:13 +0200

    Remove the demo from the index

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 import Vue from 'vue';
 import Example from './example.vue';
 
@@ -5,11 +6,11 @@ new Vue({
   el: '#app',
   render: h => h(Example),
 });
+*/
 
-/*
+
 import LControlBigImage from "./LControlBigImage.vue";
 
 export {
   LControlBigImage
 }
-*/
