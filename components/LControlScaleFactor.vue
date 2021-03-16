f784ba4a05ffa0a7298c75b9a254a6aeaf9fef9d
Ivaylo Nikolov <inikolov@sonicomit.com>
2021-03-16 15:17:54 +0200

    LControlScaleFactor - external css, loading time fix with interval

--- components/LControlScaleFactor.vue
+++ components/LControlScaleFactor.vue
@@ -1,32 +1,46 @@
 <script>
-import {
-  OptionsMixin,
-  ControlMixin,
-} from 'vue2-leaflet';
-import 'leaflet.ScaleFactor/leaflet.scalefactor.min.css';
+import { OptionsMixin, ControlMixin } from "vue2-leaflet";
+//import 'leaflet.ScaleFactor/leaflet.scalefactor.min.css';
 //import 'leaflet.ScaleFactor/leaflet.scalefactor.js';
 export default {
-  name: 'LControlScaleFactor',
-  mixins: [
-    ControlMixin,
-    OptionsMixin,
-  ],
+  name: "LControlScaleFactor",
+  mixins: [ControlMixin, OptionsMixin],
   created() {
-      let recaptchaScript = document.createElement('script')
-    recaptchaScript.setAttribute('src', 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js')
-    document.head.appendChild(recaptchaScript)
+    let recaptchaScript = document.createElement("script");
+    recaptchaScript.setAttribute(
+      "src",
+      "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"
+    );
+    document.head.appendChild(recaptchaScript);
 
-    let recaptchaScript2 = document.createElement('script')
-    recaptchaScript2.setAttribute('src', 'https://rawgit.com/MarcChasse/leaflet.ScaleFactor/master/leaflet.scalefactor.min.js')
-    document.head.appendChild(recaptchaScript2)
+    let recaptchaScript2 = document.createElement("script");
+    recaptchaScript2.setAttribute(
+      "src",
+      "https://rawgit.com/MarcChasse/leaflet.ScaleFactor/master/leaflet.scalefactor.min.js"
+    );
+    document.head.appendChild(recaptchaScript2);
+
+    let recaptchaScript3 = document.createElement("link");
+    recaptchaScript3.setAttribute(
+      "href",
+      "https://rawgit.com/MarcChasse/leaflet.ScaleFactor/master/leaflet.scalefactor.min.css"
+    );
+    recaptchaScript3.setAttribute("rel", "stylesheet");
+    recaptchaScript3.setAttribute("type", "text/css");
+    document.head.appendChild(recaptchaScript3);
   },
   mounted() {
-    this.mapObject = new L.control.scalefactor(this.options);
-    this.mapObject.addTo(this.$parent.mapObject);
+    let internval = setInterval(() => {
+      if (typeof L.control.scalefactor != "undefined") {
+        this.mapObject = new L.control.scalefactor(this.options);
+        this.mapObject.addTo(this.$parent.mapObject);
+        clearInterval(internval);
+      }
+    }, 1000);
+    //
   },
   render() {
     return null;
   },
 };
-
 </script>
